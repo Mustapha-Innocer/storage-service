a295101935d2999665f055f23abd438173256c86
Mustapha Innocer <mustaphainnocer@gmail.com>
2025-04-22 17:03:30 +0000

    created author db model

---
+++ lib/db/models/Author.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+
+from lib.db.session import Base
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    stories = relationship("Story", back_populates="author")
