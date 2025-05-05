ee45b967fe6f64698fadd96f109dd779005afe37
Mustapha Innocer <mustaphainnocer@gmail.com>
2025-05-05 23:26:37 +0000

    put kafka topics in config

--- lib/config/config.py
+++ lib/config/config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-KAFKA_SERVER = os.getenv("KAFKA_SERVER")
-KAFKA_PORT = os.getenv("KAFKA_PORT")
+KAFKA_SERVER = os.getenv("KAFKA_SERVER", "localhost")
+KAFKA_PORT = os.getenv("KAFKA_PORT", "9092")
+KAFKA_CONSUMER_TOPIC = os.getenv("KAFKA_TOPIC", "processed-data")
 
-DB_URL = os.getenv("DB_URL")
+DB_URL = os.getenv("DB_URL", "postgresql://username:password@host:port/dbname")
